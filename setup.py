a8cb935ab16346285859190e3c0c4a971eadc74f
ahmedbodi <ahmed_bodi@msn.com>
2020-01-08 12:47:37 +0000

    [Dependencies] Remove Python2.6 Support

--- setup.py
+++ setup.py
@@ -19,13 +19,12 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Ahmed Bodiwala",
     author_email='ahmedbodi@crypto-expert.com',
-    python_requires='>=2.6',
+    python_requires='>=2.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
